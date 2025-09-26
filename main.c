4960ea614c9964f72f8b5496c309d5624cc39b8a
moustapha93210 <moustapha.chaib.sio@gmail.com>
2025-09-26 02:38:03 +0200

    Validation 1.2-26/09/2025-02h37

--- main.c
+++ main.c
@@ -2,17 +2,20 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct Position {
+typedef struct Position
+{
     int x;
     int y;
 } Position;
 
-typedef enum BoatOrientation {
+typedef enum BoatOrientation
+{
     BOAT_HORIZONTAL,
     BOAT_VERTICAL
 } BoatOrientation;
 
-typedef struct Boat {
+typedef struct Boat
+{
     Position position;
     int length;
     int orientation;
@@ -20,86 +23,158 @@ typedef struct Boat {
     int sunk;
 } Boat;
 
-typedef struct {
+typedef struct
+{
 
     char **board_template;
     int board_lines;
     int board_columns;
 
-}Board;
+} Board;
 
-void printf_board(Board *board)
+void print_just_board(Board *board)
 {
-    Board board1 = {
-        .board_template = NULL,
-        .board_lines = 0,
-        .board_columns = 0      
-    };
-
-    
-    printf("Veuillez entrer le nombre de lignes du plateau : ");
-    fflush(stdin);
-    scanf("%d", &(board1.board_lines));
-
-    while(board1.board_lines > 26 )
+    for (int i = 0; i < board->board_lines + 1; i++)
     {
-        printf("Veuillez recommencez !");
-
-        printf("Veuillez entrer le nombre de lignes du plateau : ");
-        fflush(stdin);
-        scanf("%d", &(board1.board_lines));
-    }
-
-    board1.board_columns = board1.board_lines;
-
-    char **boardL = malloc(sizeof(char*) * (board1.board_lines + 1));
-
-    for(int i = 0; i < board1.board_lines+1; i++)
-    {
-        boardL[i] = malloc(sizeof(char) * (board1.board_columns + 1));
-
-        for(int j = 0; j < board1.board_columns+1; j++)
+        for (int j = 0; j < board->board_columns + 1; j++)
         {
-            if(i == 0 && j == 0){
-                boardL[i][j] = ' ';
-                printf("%c\t", boardL[i][j]);
-            }
-            else if(j == 0 && i > 0)
+            if(i > 9 && j == 0)
             {
-                boardL[i][1] = 1;
                 printf("%d\t", i);
-            }
-            else if(i == 0 && j > 0)
-            {
-                boardL[0][j] = 'A'+ (j-1);
-                printf("%c\t", boardL[0][j]);
             }
             else
             {
-                boardL[i][j] = '.';
-                printf("%c\t", boardL[i][j]); 
-            }
+                printf("%c\t", board->board_template[i][j]);
+            }   
         }
-
-        printf("\n");
         printf("\n");
     }
+}
 
+// Affiche le plateau de jeu de la bataille navale
+void printf_board(Board *board)
+{
+    printf("Veuillez entrer le nombre de lignes du plateau : ");
+    scanf("%d", &(board->board_lines));
 
-    for(int i = 0; i < board1.board_lines; i++)
+    while (board->board_lines > 26 || board->board_lines < 5)
     {
-        free(boardL[i]);
+        if (board->board_lines > 26)
+            printf("Erreur vous ne pouvez pas avoir un plateau plus grand que la taille 26. Veuillez recommencez !\n");
+        else
+            printf("Erreur vous ne pouvez pas avoir un plateau plus petit que la taille 5. Veuillez recommencez !\n");
+
+        printf("Veuillez entrer le nombre de lignes du plateau : ");
+        scanf("%d", &(board->board_lines));
     }
-    
-    free(boardL);
+
+    board->board_columns = board->board_lines;
+
+    board->board_template = malloc(sizeof(char *) * (board->board_lines + 1));
+    for (int i = 0; i < board->board_lines + 1; i++)
+    {
+        board->board_template[i] = malloc(sizeof(char) * (board->board_columns + 1));
+        for (int j = 0; j < board->board_columns + 1; j++)
+        {
+            if (i == 0 && j == 0)
+                board->board_template[i][j] = ' ';
+            else if (j == 0 && i > 0)
+                board->board_template[i][j] = '0' + i;
+            else if (i == 0 && j > 0)
+                board->board_template[i][j] = 'A' + (j - 1);
+            else
+                board->board_template[i][j] = '.';
+            //printf("%c\t", board->board_template[i][j]);
+        }
+        //printf("\n");
+    }
+    //print_just_board(board);
 }
 
 
-int main(void) {
+
+// Affichage des navires en dur
+void printf_boat(Board *board, char *type_Boat) /*, char *croiseur, char *destroyer, char *torpilleur*/
+{
+
+    if (strcmp(type_Boat, "PA") == 0)
+    {
+        int size_type_boat = 5;
+
+        for(int i = 1; i < size_type_boat + 1; i++)
+        {
+            board->board_template[1][i] = 'P';
+        }
+        
+    }
+    else if (strcmp(type_Boat, "CR") == 0)
+    {
+        int size_type_boat = 4;
+
+        for(int i = 2; i < size_type_boat + 2; i++)
+        {
+            board->board_template[i][4] = 'C';
+        }
+    }
+    else if (strcmp(type_Boat, "DE") == 0)
+    {
+        int size_type_boat = 3;
+
+        for(int i= 2; i < size_type_boat + 2; i++)
+        {
+            
+            board->board_template[i][3] = 'D';
+        }
+    }
+    else if (strcmp(type_Boat, "TO") == 0)
+    {
+        int size_type_boat = 2;
+
+        for(int j= 1; j < size_type_boat + 1; j++)
+        {
+            board->board_template[3][j] = 'T';
+        }
+    }
+        
+    print_just_board(board);
     
+}
+
+char *demande_Type_Boat()
+{
+    char *demande;
+    demande = malloc(sizeof(char) * 3);
+    printf("Veuillez entrer le type de bateau que vous souhaitez (PA, CR, DE, TO) : ");
+    scanf("%s", demande);
+
+    while (strcmp(demande, "PA") != 0 && strcmp(demande, "CR") != 0 && strcmp(demande, "DE") != 0 && strcmp(demande, "TO") != 0)
+    {
+        printf("Erreur vous ne pouvez pas entrer ce type de bateau. Veuillez recommencez !\n");
+
+        printf("Veuillez entrer le type de bateau que vous souhaitez (PA, CR, DE, TO) : ");
+        scanf("%s", demande);
+    }
+
+   return demande;
+}
+
+int main()
+{
+
     Board board2;
 
     printf_board(&board2);
-    
+
+    // char *type_Boat;
+
+    int cTB = 0;
+
+    while(cTB < 5)
+    {
+        char *type_Boat = demande_Type_Boat();
+        printf_boat(&board2, type_Boat);
+        cTB++;
+    }
+
     return 0;
 }
